fd71b9884f563294e61a429c4c95b9c09c0d3589
datnt <datnt@evolableasia.vn>
2018-11-26 16:55:53 +0700

    update comments and toolbar

--- majora-shop/src/Components/GameDetail/gameDetail.jsx
+++ majora-shop/src/Components/GameDetail/gameDetail.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import ImageGallery from 'react-image-gallery';
 import Comment from '../Comment/Comment';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceICon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
+import FaceIcon from '@material-ui/icons/Face';
+import DoneIcon from '@material-ui/icons/Done';
 
 import "react-image-gallery/styles/css/image-gallery.css";
 
@@ -18,20 +30,30 @@ const sample = {
     { original: 'https://s.vndb.org/sf/62/1062.jpg', thumbnail: 'https://s.vndb.org/sf/62/1062.jpg' },
     { original: 'https://s.vndb.org/sf/64/1064.jpg', thumbnail: 'https://s.vndb.org/sf/64/1064.jpg' },
   ],
-  price: '250000',
+  price: '250.000 VND',
   comments: [
-    { comment:{name: 'nanami', avatar:'https://myanimelist.cdn-dena.com/images/characters/14/310999.jpg'}, comment: 'this game is worth the price' },
-    { comment:{name: 'nanami', avatar:'../../Img/Users/nanami.jpg'}, comment: 'this game is worth the price' },
-    { comment:{name: 'nanami', avatar:'../../Img/Users/nanami.jpg'}, comment: 'this game is worth the price' },
-    { comment:{name: 'nanami', avatar:'../../Img/Users/nanami.jpg'}, comment: 'this game is worth the price' }
-  ]
+    { user: { name: 'nanami', avatar: 'https://myanimelist.cdn-dena.com/images/characters/14/310999.jpg' }, content: 'this game is worth the price' },
+    { user: { name: 'nanami', avatar: 'https://myanimelist.cdn-dena.com/images/characters/14/310999.jpg' }, content: 'For all the old D&D players out there: Darkest Dungeon is a digitally enhanced and massively prolonged interpretation of Tomb of Horrors. For anyone else: Darkest Dungeon is a soulcrushingly deadly dungeon crawler with absolutely fantastic gameplay, depressing art design, massive content and very polished turn-based party-of-four battles. One of the most (not) fun games I have played in a long while. Prepare to die a lot. Prepare to lose your most favourite characters every few missions. Prepare to watch your characters lose their minds. Prepare to abandon quests regularly. Prepare to suffer chronically from a lack of supplies, lack of equipment, lack of torches and pretty much lack of anything you would ever need to succeed in your missions. This game is pure bliss and at the same time the most depressing piece of software I have ever laid eyes upon. I pull my hat towards the developers for making this masterpiece.' },
+    { user: { name: 'nanami', avatar: 'https://myanimelist.cdn-dena.com/images/characters/14/310999.jpg' }, content: 'this game is worth the price' },
+    { user: { name: 'nanami', avatar: 'https://myanimelist.cdn-dena.com/images/characters/14/310999.jpg' }, content: "I noticed a lot of reviews citing the game as ‘too hard’ or ‘all about rng’ and I can tell you exactly why: those players were expecting a generic dungeon crawler where you can kerbstomp every boss with high level and super gear, and the managerial risk mitigation required in Darkest Dungeon (DD) is foreign and alien to them. All the RPG nowadays features the player as this, omnipotent chosen one who is out to save the world single handly, DD subverts that 100%. Your team sports heros with mortal characteristics of strength and weakness, vices and fears that is nothing like the “chosen one out to save the world”. That is fine, we have seen that in lores before but here is the kicker - You got to manage those flaws in DD! In fact, if you have worked in a corporate environment DD will be a piece of cake for you, since it is all about risk assessment and human resource (HR) management. What about the rng? Let's cover the risk assessment: to put it simply, you look at the LIKELIHOOD (rng) and CONSEQUENCE of a RISK to get a risk score, and you assign CONTROLS to reduce the two aforementioned factors. Say if my vestel is highly prone to stress in dark due to quirks, we have “RISK” of her getting psyched out and become incapable to heal. The “CONSEQUENCE” of that would be wiping and losing the juicy trinkets your squad has (catastrophic) and the chance of me having no torch may occur in long dungeons (possible). So now, the typical gamers have two response : Vestel heal is weak, or negative quirks are too strong. They look at problems NOT solutions. To a thinking gamer, a CONTROL will be applied. I can reduce the likelihood (rng) by -Curing the quirk (if time permits) -Bring more torches -Have a man of arms to guard her -Have ranged damage that can wipe out stressors on round 1, IE dogs and highwayman To reduce the consequence by -Strict retreat protocol- if the healer is not coping, escape combat -Have a crusader that can reduce stress -Have stress reduction camping skills -Have self heal on dps to reduce healer workload Now we have applied control, we have reduced the likelihood (rng) of healer getting psyched out from possible to unlikely level. And even if the healer does get psyched out we have controls to reduce the consequences from catastrophic to minor (if they retreat they go to town have a break no biggie). It is understandable that risk management skill do not exist in the immature adults or children who have been fed with hero complex and unable to accept compromise, it is easier to just blame it on rng. While the game does have certain shortfalls like slow combat animation, overall it has pioneered a new type of dungeon crawler that's a fresh breeze compared to the old and outdated ‘grind to max level and save the world’ model. In Conclusion: Not saying you have to like the game or agree to the review, but this is just not the typical game where god gear will win you 100% of the games, and if challenging games isn't your thing, you may need to reconsider purchasing it. However if you do enjoy real challenges like me, you are in for helluva treat!" }
+  ],
+  tags: [
+    'bookmarks', 'built-in', 'hints', 'ClubFloyd', 'transcript', 'collaboration commercial', 'cover art', 'CYOA', 'desktop', 'dragon', 'Eamon', 'fantasy', 'female protagonist', 'first person', 'foreign language', 'gender-neutral', 'protagonist', 'german', 'graphics', 'horror', 'humor']
 }
 
 class GameDetail extends Component {
   state = {
     images: sample.images
   }
+
   render() {
+    function handleDelete() {
+      alert('You clicked the delete icon.'); // eslint-disable-line no-alert
+    }
+
+    function handleClick() {
+      alert('You clicked the Chip.'); // eslint-disable-line no-alert
+    }
     const { classes, title, images, description, price } = this.props;
     return (
       <div>
@@ -42,25 +64,57 @@ class GameDetail extends Component {
           <br />
           <br />
           <Grid container spacing={24}>
-            <Grid item xs={12} sm={9}>
+            <Grid item xs={12} sm={8}>
 
               <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                 <ImageGallery items={images ? images : sample.images} useTranslate3D={true} />
               </div>
               <br />
               <br />
+              <Card className={classes.card}>
               <Typography variant="h6" component="p">
                 {description ? description : sample.description}
               </Typography>
+              <br />
+              </Card>
               <Typography variant="h4" component="h3">Reviews</Typography><br /><br />
-              {Object.keys(sample.comments).map((key, value) => 
-                <Comment comment={sample.comments[key]}/>
+              {Object.keys(sample.comments).map((key, value) =>
+                <Comment comment={sample.comments[key]} />
               )}
             </Grid>
-            <Grid item xs={12} sm={3}>
-              <Typography variant="h4" component="h3">
-                {price ? price : sample.price}
-              </Typography>
+            <Grid item xs={12} sm={4}>
+              <Card className={classes.card}>
+                <Typography variant="h4" component="h3">
+                  {price ? price : sample.price}
+                </Typography>
+                <Button variant="contained" color="secondary" className={classes.button}>
+                  Delete
+        <DeleteIcon className={classes.rightIcon} />
+                </Button>
+                <Button variant="contained" color="primary" className={classes.button}>
+                  BUy
+        <Icon className={classes.rightIcon}>send</Icon>
+                </Button>
+                <Button variant="contained" color="default" className={classes.button}>
+                  Upload
+        <CloudUploadIcon className={classes.rightIcon} />
+                </Button>
+                <Button variant="contained" size="small" className={classes.button}>
+                  <SaveIcon className={classNames(classes.leftIcon, classes.iconSmall)} />
+                  Save
+      </Button>
+
+                <div className={classes.chipContainer}>
+                  <Chip
+                    label="Visual Novel"
+                    className={classes.chip}
+                    component="a"
+                    color="primary"
+                    href="#chip"
+                    clickable
+                  />
+                </div>
+              </Card>
             </Grid>
 
           </Grid>
@@ -77,6 +131,33 @@ const styles = darkBaseTheme => ({
     paddingTop: darkBaseTheme.spacing.unit * 2,
     paddingBottom: darkBaseTheme.spacing.unit * 2,
     flexGrow: 1,
+  },
+  card: {
+    maxWidth: "100%",
+    fontSize: '1rem',
+    margin: '10px',
+    backgroundColor: '#61bafb30',
+    padding: '20px'
+  },
+  button: {
+    margin: darkBaseTheme.spacing.unit,
+  },
+  leftIcon: {
+    marginRight: darkBaseTheme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: darkBaseTheme.spacing.unit,
+  },
+  iconSmall: {
+    fontSize: 20,
+  },
+  chip: {
+    margin: darkBaseTheme.spacing.unit,
+  },
+  chipContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
   },
 });
 
