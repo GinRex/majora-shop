46c871724529d473e31873b8ac3400eddb034605
nhmdat <38955345+nhmdat@users.noreply.github.com>
2018-11-26 21:33:34 +0700

    edit value

--- majora-shop-screen-3-4/src/products/ThumbnailField.js
+++ majora-shop-screen-3-4/src/products/ThumbnailField.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ThumbnailField = ({ record }) => <img src={record.thumbnail} style={{ width: 25, maxWidth: 25, maxHeight: 25 }} alt="" />;
+const ThumbnailField = ({ record }) => <img src={record.thumbnail} style={{ width: 50, maxWidth: 50, maxHeight: 50 }} alt="" />;
 
 ThumbnailField.defaultProps = {
-    style: { padding: '0 0 0 16px' },
+    style: { padding: '0 0 0 10px' },
 };
 
 export default ThumbnailField;
